a32e2eed95f704cfa0629565f22f93f4fc671cba
0hyeon <djdjdjk2006@naver.com>
2023-05-01 22:07:16 +0900

    feat: 큐구현

--- 연결리스트/practice.js
+++ 연결리스트/practice.js
@@ -4,17 +4,41 @@ class Node {
     this.next = null;
   }
 }
-
-class linkedList {
+class singlyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
   }
   find(value) {
-    let current = this.head;
-    while (current.value !== value) {
-      //head가 null이 아닌경우 반복문 진행
-      //head value랑 다르면 같을때까지 반복
+    let curnNode = this.head;
+    while (curnNode.value !== value) {
+      curnNode = curnNode.next;
+    }
+    return curnNode;
+  }
+  append(newValue) {
+    const newNode = new Node(newValue);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode; //이어붙이기
+      this.tail = newNode;
     }
   }
+  insert(node, newValue) {
+    const newNode = new Node(newValue);
+    newNode.next = node.next;
+    node.next = newNode;
+  }
+  remove(value) {
+    let prevNode = this.head;
+    while (prevNode.next.value !== value) {
+      prevNode = prevNode.next;
+    }
+    if (prevNode !== this.head) {
+      prevNode.next = prevNode.next.next;
+    }
+  }
+  display() {}
 }
